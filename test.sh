5b2f976b1684799239a3f4f61b668ddac6c33fe0
samhith <samhith.mr@gmail.com>
2025-03-31 10:30:00 +0530

    configured backend and frontend folder

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-DATE="2025-03-28T10:30:00"
+DATE="2025-03-31T10:30:00"
 commit_with_message() {
   GIT_AUTHOR_DATE="$DATE" GIT_COMMITTER_DATE="$DATE" git commit -m "$1"
 }
 
-commit_with_message "setup basic react typescript project"
+commit_with_message "configured backend and frontend folder"
